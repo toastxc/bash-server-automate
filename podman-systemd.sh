6860676b5c73c5f9f0ccab5f5fe3704936785bd6
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-09-24 13:28:11 +0800

    Create podman-systemd.sh

---
+++ podman-systemd.sh
@@ -0,0 +1,36 @@
+echo "choose a container"
+read container
+
+podman generate systemd \
+	--start-timeout=60        \
+	--stop-timeout=60	\
+	$container > $container.service
+
+echo "push to systemd?"
+
+read answer
+
+
+list=$(ls /etc/systemd/system/$container.service )
+
+if [[ $list = "/etc/systemd/system/$container.service" ]]; then
+
+	echo "service with this name already exists, removing..."
+	sudo rm /etc/systemd/system/$container.service
+	sudo rm $HOME/.config/systemd/user/$container.service
+fi
+
+
+if [[ $answer != "y" ]]; then 
+
+	echo "exiting..."
+
+	exit
+fi
+
+sudo cp $container.service /etc/systemd/system/
+sudo cp $container.service $HOME/.config/systemd/user/
+
+sudo systemctl daemon-reload
+echo ""
+systemctl --user enable --now $container.service &
