eccf8b1970274fc47aa01cc57d218e9ab5438bcd
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-09-24 13:43:09 +0800

    Update fedora-network-legacy.sh

--- fedora-network-legacy.sh
+++ fedora-network-legacy.sh
@@ -1,34 +1,26 @@
-if (( $EUID != 0 )); then
-        echo "Please execute as root."
-        exit
-fi
-
-
-
-if (($OSTYPE != "linux-gnu")); then
-        echo "This script is for GNU/Linux only."
-        exit
-fi
-
 DNF=$(ls /etc/ | grep dnf);
 
-if (( $DNF != "dnf")); then
-        echo "This script only works for Fedora based systems."
-        exit
-fi      
+if (( $EUID == 0 )); then
+		if (( $DNF == "dnf")); then
+			
+			echo "Fedora Network Fixer"
+			echo "This program will set Fedora networking back to legacy (workstation23)\n"
+			echo "THIS PROGRAM DOES NOT COME WITH ANY WARRENTY OR WAY TO RESET FEDORA NETWORKING"
+			echo "Do you wish to continue? Y/n"
 
+			read YorN
+			
+			if [ $YorN = "n" ]; then
+				exit
+			else
+				dnf install crypto-policies-scripts -y
+				
+				update-crypto-policies --set DEFAULT:FEDORA32
 
-echo "Fedora Network Fixer"
-echo "This program will set Fedora networking back to legacy (workstation23)\n"
-echo "THIS PROGRAM DOES NOT COME WITH ANY WARRENTY OR WAY TO RESET FEDORA NETWORKING"
-echo "Do you wish to continue? Y/n"
-
-read YorN
-
-if [ $YorN = "n" ]; then
-        exit
+			fi
+		else
+			echo "This script only works on Fedora based systems"
+		fi
+	else
+		echo "This program requres root"
 fi
-
-dnf install crypto-policies-scripts -y
-
-update-crypto-policies --set DEFAULT:FEDORA32
